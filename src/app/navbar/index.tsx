e02238498a3d834e1a34fccdd6d807b36fa68b33
samiShah8 <shahsamiksha54@6mail.com>
2024-08-15 17:47:52 +0545

    navbar with mobileview completed

--- src/app/navbar/index.tsx
+++ src/app/navbar/index.tsx
@@ -1,47 +1,158 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { AiOutlineClose } from "react-icons/ai";
 import Logo from "../../../public/assets/logo/ransaini.png";
+
 function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const toggleNav = () => {
+    setShowNavbar(!showNavbar);
+  };
+  const [activeNav, setActiveNav] = useState("home");
   return (
-    <header className=" overflow-hidden">
-      <div className="header__container flex items-center mx-4 justify-around border-border-yellow-600 bottom-2 mb-1 ">
-        <div className="w-24 py-2">
+    <header className="overflow-hidden">
+      <div className="header__container flex items-center justify-between mx-4 py-2">
+        <div className="w-20">
           <Image src={Logo} alt={"Technical Ranasaini Logo"} />
         </div>
-        <nav className=" invisible ">
-          <a className="nav-font" href="#">
+        <nav className="hidden lg:flex flex-1 items-center gap-20 px-20 text-[0.875rem] leading-[1.7]">
+          <a
+            onClick={() => setActiveNav("home")}
+            className={`${
+              activeNav === "home"
+                ? "text-black-800 font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
             home
           </a>
-          <a className="nav-font" href="#">
+          <a
+            onClick={() => setActiveNav("services")}
+            className={`${
+              activeNav === "services"
+                ? "text-black font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
             services
           </a>
-          <a className="nav-font" href="#">
+          <a
+            onClick={() => setActiveNav("contact")}
+            className={`${
+              activeNav === "contact" ? "text-black font-bold" : "text-gray-800"
+            }`}
+            href="#"
+          >
             contact
           </a>
-          <a className="nav-font" href="#">
+          <a
+            onClick={() => setActiveNav("about")}
+            className={`${
+              activeNav === "about" ? "text-black font-bold" : "text-gray-800"
+            }`}
+            href="#"
+          >
             about
           </a>
-          <button className="">Build with us</button>
+          <a
+            onClick={() => setActiveNav("resources")}
+            className={`${
+              activeNav === "resources"
+                ? "text-black-800 font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
+            resources
+          </a>
         </nav>
-        <div>
-          <GiHamburgerMenu className="h-4 w-4" />
-        </div>
-      </div>
-      <div className="mobile-nav flex flex-col gap-4 bg-header  items-center text-[0.875rem] leading-[1.7]">
-        <a className="text" href="#">
-          home
-        </a>
-        <a href="#">services</a>
-        <a href="#">contact</a>
-        <a href="#">about</a>
-        <div className="w-[90%]">
-          <button className="text-center text-white rounded-md w-full bg-body-color px-2 py-2 text-xl font-bold uppercase">
+        <div className="hidden lg:block">
+          <button className="text-center rounded-md bg-body-color w-48 h-10 hover:bg-orange-500 text-white">
             Build with us
           </button>
         </div>
+        <div className="md:hidden">
+          {showNavbar ? (
+            <AiOutlineClose
+              className="h-6 w-6 cursor-pointer transition duration-150 ease-in-out"
+              onClick={toggleNav}
+            />
+          ) : (
+            <GiHamburgerMenu
+              className="h-6 w-6 cursor-pointer "
+              onClick={toggleNav}
+            />
+          )}
+        </div>
       </div>
+
+      {showNavbar && (
+        <div className="mobile-nav flex flex-col gap-4 bg-header items-center text-[1rem] leading-[1.7] md:hidden">
+          <a
+            onClick={() => setActiveNav("home")}
+            className={`${
+              activeNav === "home"
+                ? "text-black-800 font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
+            home
+          </a>
+          <a
+            onClick={() => setActiveNav("services")}
+            className={`${
+              activeNav === "services"
+                ? "text-black font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
+            services
+          </a>
+          <a
+            onClick={() => setActiveNav("contact")}
+            className={`${
+              activeNav === "contact" ? "text-black font-bold" : "text-gray-800"
+            }`}
+            href="#"
+          >
+            contact
+          </a>
+          <a
+            onClick={() => setActiveNav("about")}
+            className={`${
+              activeNav === "about" ? "text-black font-bold" : "text-gray-800"
+            }`}
+            href="#"
+          >
+            about
+          </a>
+          <a
+            onClick={() => setActiveNav("resources")}
+            className={`${
+              activeNav === "resources"
+                ? "text-black-800 font-bold"
+                : "text-gray-800"
+            }`}
+            href="#"
+          >
+            resources
+          </a>
+          <div className="w-[90%]">
+            <button className="text-center text-white rounded-md w-full hover:bg-orange-500 bg-body-color px-2 py-2 text-1.1rem font-bold uppercase">
+              Build with us
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
+
 export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,12 @@ const config: Config = {
         "heding-weight": "700",
         "headin-weight2": "500",
       },
+      // screens: {
+      //   sm: "480px",
+      //   md: "768px",
+      //   lg: "1024px",
+      //   xl: "1280px",
+      // },
     },
   },
   plugins: [],
